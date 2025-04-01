3d513985f09ea038ff9b632efe88682be0125078
Kleber Wagner Junior <kleberwagner107@gmail.com>
2025-04-01 09:36:27 -0300

    Criei o 'exercise 02' e criei um sistema que pega dois números e da a soma, subtração, multiplicação e divisão.

---
+++ Exercise 02.py
@@ -0,0 +1,11 @@
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+print(" ")
+soma = (n1 + n2)
+subtracao = (n1 - n2)
+multiplicacao = (n1 * n2)
+divisao = (n1 / n2)
+print(f"O valor da soma é {soma}\n"
+      f"O valor da subtação é {subtracao}\n"
+      f"O valor da multiplicação é {multiplicacao}\n"
+      f"O valor da divisão é {divisao}")
