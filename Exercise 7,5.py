8e95ca1939d2e5ce714d751dff381894028d6fab
Kleber Wagner Junior <kleberwagner107@gmail.com>
2025-04-03 09:19:28 -0300

    fiz algumas alterações no 'exercise 07' e criei o 'exercise 7,5' para fazer uma variação DO ANTERIOR7

--- Exercise 7,5.py
+++ Exercise 7,5.py
@@ -1,12 +1,12 @@
 print("Digite o tipo de combustivel utilizado")
-combust = input("G - Gasolina\nE - Etanol\nEscolha uma das opções: ")
+combust = input("G - Gasolina\nE - Etanol\nEscolha uma das opções: ").upper()
 litros = float(input("Digite quantos litros você abasteceu: "))
 etanol = 4.90
 gasolina = 5.80
 print(" ")
-if combust == "G" or combust == "g":
+if combust == "G":
     print(f"O valor a ser pago será R${litros * gasolina:.2f} de gasolina")
-elif combust == "E" or combust == "e":
+elif combust == "E":
     print(f"O valor a ser pago será R${litros * etanol:.2f} de etanol")
 else:
-    print(f"O combustivel está indisponivel")
+    print(f"O combustivel selecionado está indisponivel")
